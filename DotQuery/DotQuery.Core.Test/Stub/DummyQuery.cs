6a2ff69bc5c008902193e15d3acfed20dcfde823
karldodd <karldodd@gmail.com>
2013-07-30 15:58:21 +0800

    Add project DotQuery.Core.Test

---
+++ DotQuery/DotQuery.Core.Test/Stub/DummyQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotQuery.Core.Test.Stub
+{
+    public class DummyQuery : QueryBase
+    {
+        
+    }
+}
---
+++ DotQuery/DotQuery.Core.Test/TestQueryExecutor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotQuery.Core.Test
+{
+    [TestClass]
+    public class TestQueryExecutor
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
