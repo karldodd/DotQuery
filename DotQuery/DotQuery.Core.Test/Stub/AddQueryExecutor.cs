a30f530edd4f6c4ecfad79edf05e4b90ba015cee
karldodd <karldodd@gmail.com>
2013-07-31 13:09:24 +0800

    add the missing test class AddQueryExecutor

---
+++ DotQuery/DotQuery.Core.Test/Stub/AddQueryExecutor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotQuery.Core.Test.Stub
+{
+    public class AddQueryExecutor : QueryExecutor<AddQuery, int>
+    {
+        private readonly TimeSpan m_delayTime;
+
+        public AddQueryExecutor(TimeSpan delayTime) : base(new SimpleQueryCache(new DefaultQueryEqualityComparer()))
+        {
+            m_delayTime = delayTime;
+        }
+
+        protected override async Task<int> DoQueryAsync(AddQuery query)
+        {
+            await Task.Delay(m_delayTime);
+            return query.Left + query.Right;
+        }
+    }
+}
