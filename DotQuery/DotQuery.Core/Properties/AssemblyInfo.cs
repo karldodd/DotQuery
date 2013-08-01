1d522a9ca29a0d69daab1bbc7e305a865b7f5c5c
karldodd <karldodd@gmail.com>
2013-08-01 12:54:38 +0800

    use unified assemblyinfo file for DotQuery.Core

--- DotQuery/DotQuery.Core/Properties/AssemblyInfo.cs
+++ DotQuery/DotQuery.Core/Properties/AssemblyInfo.cs
@@ -14,6 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+#if portable
+#else
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -21,6 +23,8 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5c26ab71-321a-4228-a724-fb63f352db12")]
+#endif
+
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
